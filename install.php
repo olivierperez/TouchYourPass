1ddce875ad9b553f38884c88cdc6cd796528323c
Olivier PEREZ <olivier@olivierperez.fr>
2015-05-18 00:00:58 +0200

    Install: Redirect to Home when the app is already installed

--- install.php
+++ install.php
@@ -1,5 +1,6 @@
 <?php
 
+use TouchYourPass\Utils;
 
 define('ROOT_DIR', dirname(__FILE__));
 
@@ -8,6 +9,11 @@ require ROOT_DIR . '/inc/const.php';
 require ROOT_DIR . '/inc/i18n.php';
 require ROOT_DIR . '/vendor/o80/i18n/src/shortcuts.php';
 require ROOT_DIR . '/inc/smarty.php';
+
+if (file_exists(ROOT_DIR. '/inc/conf.php')) {
+    header(('Location: ' . Utils::serverUrl()));
+    exit;
+}
 
 $smarty->assign('currentPage', 'install');
 $smarty->assign('loggedIn', false);
